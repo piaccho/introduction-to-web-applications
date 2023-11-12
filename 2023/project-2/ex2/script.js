6dc473ac83153e1e228865721c2735283cba6e5c
piacho <sebi77styp@gmail.com>
2023-11-12 17:24:02 +0100

    project-2

--- 2023/project-2/ex2/script.js
+++ 2023/project-2/ex2/script.js
@@ -1,16 +1,21 @@
 // TODO
-// 1. change health bar to lives
-// 2. change points to score
+// 2. change score to score
 // 3. add zombie move speed
 // 4. add zombie spawn interval
+// 5. add zombie hit animation
+// 6. add zombie death animation
+// 8. fix click button shot
+// 9. fix blinking lives bar animation
+// 10. add score of shooting and killing zombies
 
 const cursor = document.querySelector('#cursor');
+const totalScoreEndGame = document.querySelector('#total-score-value');
 const ammoAmount = document.querySelector('#ammo-amount');
-const healthBar = document.querySelector('#health-bar');
-const pointsCounter = document.querySelector('#points-counter');
+const livesBar = document.querySelector('#lives-bar');
+const score = document.querySelector('#score');
 const mainMenu = document.querySelector('#main-menu');
 const gameOverScreen = document.querySelector('#game-over');
-const startButton = document.querySelector('#start-btn');
+const gameButtons = document.querySelectorAll('.game-btn');
 const gameContainer = document.querySelector('#game-container');
 
 const CURSOR_IMG_SIZE = window.innerWidth * 0.2;
@@ -20,8 +25,8 @@ const BULLET_HOLE_DISAPPEAR_START = 3000;
 const FADE_DELAY = 100;
 const RELOAD_DELAY = 500;
 const MAX_AMMO_AMOUNT = 7;
-const MAX_HEALTH = 3;
-const START_POINTS = 30;
+const MAX_LIVES = 3;
+const START_SCORE = 30;
 // const ZOMBIE_MOVE_SPEED = 5;
 // const ZOMBIE_SPAWN_INTERVAL = 1000;
 
@@ -29,12 +34,17 @@ let shotingAvailable = false;
 let gameRunning = false;
 const stats = {
     ammo: MAX_AMMO_AMOUNT, 
-    health: MAX_HEALTH,
-    points: START_POINTS,
+    lives: MAX_LIVES,
+    score: START_SCORE,
+    printScore: () => {
+        return String(START_SCORE).padStart(5, '0');
+    }
 }
 
+
 const moveCursor = (e) => {
-    document.querySelector("body").style.cursor = 'none';
+    document.querySelector("body").classList.add('aim-mode');
+    cursor.style.display = 'block';
     cursor.style.transform = `translate3d(${e.clientX - CURSOR_IMG_SIZE / 2}px, ${e.clientY - CURSOR_IMG_SIZE / 2}px, 0)`;
 }
 
@@ -81,23 +91,24 @@ const shot = (e) => {
 }
 
 const loadInterface = () => {
-    for (let i = 0; i < MAX_HEALTH; i++) {
-        const health = document.createElement('div');
-        health.classList.add('health');
-        healthBar.appendChild(health);
+    for (let i = 0; i < MAX_LIVES; i++) {
+        const liveHeart = document.createElement('div');
+        liveHeart.classList.add('full-heart');
+        livesBar.appendChild(liveHeart);
     }
     ammoAmount.innerHTML = stats.ammo;
-    pointsCounter.innerHTML = String(START_POINTS).padStart(5, '0');
+    score.innerHTML = stats.printScore();
 };
 
 
 const spawnZombie = () => {
-    if (gameRunning && stats.points >= 0) {
+    if (gameRunning && stats.score >= 0) {
         const zombie = document.createElement('div');
         const randomZombieSize = Math.floor(Math.random() * 0.6) + 0.7;
         zombie.className = 'zombie';
         // zombie.style.height = `${Math.floor(Math.random() * 50) + 30}px`;
-        zombie.style.bottom = `${Math.floor(Math.random() * (gameContainer.clientHeight - 50))}px`;
+        // zombie.style.bottom = `${Math.floor(Math.random() * (gameContainer.clientHeight - 50))}px`;
+        zombie.style.bottom = `${gameContainer.clientHeight}px`;
         zombie.style.left = `${gameContainer.clientWidth}px`;
         gameContainer.appendChild(zombie);
     }
@@ -107,21 +118,30 @@ const moveZombies = () => {
     if (gameRunning) {
         const zombies = document.querySelectorAll('.zombie');
         zombies.forEach(zombie => {
-            const zombieSpeed = Math.floor(Math.random() * 5) + 1;
+            const zombieSpeed = Math.floor(Math.random() * 50) + 1;
             const currentLeft = parseFloat(zombie.style.left);
             zombie.style.left = `${currentLeft - zombieSpeed}px`;
 
             // Check for collision with left edge of the game container
             if (currentLeft - zombieSpeed < 0) {
                 zombie.remove();
-                stats.health--;
+                decreaseLives();
             }
         });
+    }
+}
 
-        // Check if the game should end
-        if (stats.points < 0 || stats.health === 0) {
-            endGame();
-        }
+const decreaseLives = () => { 
+    stats.lives--;
+    const liveHearts = document.querySelectorAll('.full-heart');
+
+    if (liveHearts.length > 0) {
+        lastHeart = liveHearts[liveHearts.length - 1];
+        lastHeart.classList.remove('full-heart');
+        lastHeart.classList.add('empty-heart');
+    }
+    if (stats.lives <= 0) {
+        endGame();
     }
 }
 
@@ -130,30 +150,44 @@ const clearZombies = () => {
     zombies.forEach(zombie => zombie.remove());
 }
 
-const endGame = () => {
-    gameRunning = false;
-    gameContainer.style.display = 'none';
-    gameOverScreen.style.display = 'flex';
-    clearZombies();
-    window.removeEventListener('mousemove', moveCursor);
-    window.removeEventListener('click', shot);
-    mainMenu.style.display = 'flex';
+const gameOverLivesAnimation = () => {
+    livesBar.style.animation = 'blink 3s linear infinite';
+    setTimeout(() => {
+        livesBar.style.animation = 'none';
+    }, 3000);
 }
 
+const endGame = () => {
+    gameRunning = false;
+    window.removeEventListener('click', shot);
+    gameOverLivesAnimation();
+    setTimeout(() => {
+        totalScoreEndGame.innerHTML = stats.printScore();
+        window.removeEventListener('mousemove', moveCursor);
+        document.querySelector("body").classList.remove('aim-mode');
+        cursor.style.display = 'block';
+        gameContainer.style.display = 'none';
+        gameOverScreen.style.display = 'flex';
+        clearZombies();
+    }, 3000);
+}
 
-startButton.addEventListener('click', () => {
-    if(!gameRunning) {
+const initGame = (e) => {
+    e.stopPropagation();
+    if (!gameRunning) {
         gameRunning = true;
         gameContainer.style.display = 'block';
         mainMenu.style.display = 'none';
 
         loadInterface();
         window.addEventListener('mousemove', moveCursor)
-        shotingAvailable = true;
         window.addEventListener('click', shot);
+        shotingAvailable = true;
 
         setInterval(spawnZombie, 1000);
         setInterval(moveZombies, 100);
     };
-});
+}
+
+gameButtons.forEach(buttom => buttom.addEventListener('click', initGame));
 
