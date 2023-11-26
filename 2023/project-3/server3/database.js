a7f1c8b73610a0fed9ada4f8ecfdb7e310b5d816
Sebastian Piaskowy <sebi77styp@gmail.com>
2023-11-26 12:52:50 +0100

    project-3

---
+++ 2023/project-3/server3/database.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } =  require('sequelize');
+
+var sequelize = new Sequelize("test", "root", "", {
+    host: "localhost",
+    dialect: "sqlite",
+    operatorsAliases: false,
+    storage: "./data/database.sqlite",
+});
+
+var PersonSchema = sequelize.define(
+    "Person",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: DataTypes.STRING,
+        surname: DataTypes.STRING,
+        job: DataTypes.STRING,
+    },
+    {}
+);
+
+module.exports = {sequelize, PersonSchema};
